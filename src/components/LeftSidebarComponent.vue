8ae43ce778fd16c2dbb9160791a1e45c003075ca
sphirye <principe2366@gmail.com>
2021-06-06 17:37:21 -0400

    Sidebar icons size fixed

--- src/components/LeftSidebarComponent.vue
+++ src/components/LeftSidebarComponent.vue
@@ -1,11 +1,11 @@
 <template>
-    <v-navigation-drawer dark permanent stateless app width="115px" class="grey darken-4">
+    <v-navigation-drawer permanent stateless app width="100px" class="grey darken-4">
         <v-layout column fill-height>
             <v-list dense>
                 <v-list-item v-for="(icon, key) in sidebarTopIcons" :key="key">
                     <router-link class="ma-lg-2 d-flex justify-center full-width rotating" :to="`${icon.target}`">
                         <v-btn icon>
-                            <v-icon size="40%">${{icon.icon}}</v-icon>
+                            <v-icon x-large>${{icon.icon}}</v-icon>
                         </v-btn>
                     </router-link>
                 </v-list-item>
@@ -16,7 +16,7 @@
                 <v-list-item class="d-flex justify-center mb-lg-5" v-for="(icon, key) in sidebarBottomIcons" :key="key">
                     <router-link class="ma-lg-2 d-flex justify-center full-width rotating" :to="`/torrents/${icon.target}`">
                         <v-btn icon>
-                            <v-icon size="40%">${{icon.icon}}</v-icon>
+                            <v-icon x-large>${{icon.icon}}</v-icon>
                         </v-btn>
                     </router-link>
                 </v-list-item>
